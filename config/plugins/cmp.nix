efb12016b770a1213f5397997babd6f30924854f
Thomas Stachl <i@t5.st>
2024-07-28 08:54:01 +0200

    finally found how to add the sources to cmp

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,5 +1,6 @@
 {
   plugins.cmp.enable = true;
+  plugins.cmp.autoEnableSources = true;
   plugins.cmp.settings.mapping = {
     "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
     "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
@@ -25,10 +26,19 @@
       end, { 'i', 's' })
     '';
   };
+  plugins.cmp.settings.sources = [
+    { name = "nvim_lsp"; }
+    { name = "luasnip"; }
+    { name = "path"; }
+    { name = "buffer"; }
+    { name = "friendly-snippets"; }
+  ];
 
   plugins.luasnip.enable = true;
   plugins.cmp_luasnip.enable = true;
   plugins.cmp-nvim-lsp.enable = true;
+  plugins.cmp-nvim-lsp-document-symbol.enable = true;
+  plugins.cmp-nvim-lsp-signature-help.enable = true;
   plugins.cmp-path.enable = true;
   plugins.friendly-snippets.enable = true;
 }
